5025c9367c97840d511cbc00ba3a428808fce4e2
huynhquocsach1976-spec <huynhquocsach1976@gmail.com>
2026-09-09 16:14:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Private Financial AI Agent")
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", 
-    "postgresql://postgres.xyz:Abc@1234@aws-0.pooler.supabase.com:6543/postgres
+    "postgresql://postgres.xyz:Matkhau@281277@aws-0.pooler.supabase.com:6543/postgres
 )
 
 def get_db_connection():
